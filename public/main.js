558a8bf149459e8f86ebd4f90edd96c5eadcc123
Olivia Cunningham <Liv.cunninghamgb@gmail.com>
2019-05-20 01:38:30 -0400

    ruined my launch data . moral of the story, I did not commit

--- public/main.js
+++ public/main.js
@@ -1,7 +1,43 @@
+let spaceX = {}
+let index = 0
+
+const getHeroImage = () => {
+  fetch(' https://sdg-astro-api.herokuapp.com/api/Nasa/apod ')
+    .then(resp => {
+      return resp.json()
+    })
+    .then(message => {
+      console.log(message)
+      document.querySelector('.hero-image').style.backgroundImage =
+        'url(' + message.url + ')'
+      document.querySelector('.copy-right').innerHTML =
+        'copyright: ' + message.copyright + ' | title: ' + message.title
+    })
+}
+
+const getFlightData = () => {
+  fetch('https://sdg-astro-api.herokuapp.com/api/SpaceX/launches/upcoming')
+    .then(resp => {
+      return resp.json()
+    })
+    .then(missions => {
+      console.log(missions)
+      document.querySelector('.launch-card').textContent =
+        missions + [''] + missions.mission_Name
+    })
+    .then(missions => {
+      // console.log(missions.index]launch_site.site_name_long)
+    })
+  spaceX = missions
+}
+const showCurrentLaunchTitle = () => {
+  document.querySelector('.launch-title').textContent = spaceX.mission_Name
+}
+
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+  getHeroImage()
+  getFlightData()
+  showCurrentLaunchTitle()
 }
 
 document.addEventListener('DOMContentLoaded', main)
